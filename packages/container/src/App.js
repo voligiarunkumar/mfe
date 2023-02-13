55d774d467acb5a1666ef6bc129a32d2a22b29f5
unknown <voligiarunkumar9912345@gmail.com>
2023-02-13 14:17:54 +0530

    added css error

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider,createGenerateClassName } from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
+const generateClassName=createGenerateClassName(
+  {
+    productionPrefix:'ca'
+  }
+)
 export default () => {
   return (
     <BrowserRouter>
+    <StylesProvider createGenerateClassName={generateClassName} >
     <div>
       <Header></Header>
       <MarketingApp></MarketingApp>
     </div>
+    </StylesProvider>
     </BrowserRouter>
   );
 };
